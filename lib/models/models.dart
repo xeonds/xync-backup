d846c40b0f7f58ed2a4b31d0025a5fd68e001bed
xeonds <xeonds@stu.xidian.edu.cn>
2024-02-17 14:25:52 +0800

    Add path_provider dependency and implement a generic DB class

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
+
 class SyncFolder {
   final String id;
   final String name;
@@ -67,4 +73,85 @@ enum SyncMethod {
   deleteAfterDownload,
   uploadMirror,
   downloadMirror,
+}
+
+class DB<T> {
+  late String _fileName;
+
+  DB(String fileName) {
+    _fileName = fileName;
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File('${directory.path}/$_fileName');
+  }
+
+  Future<List<T>> readAll() async {
+    try {
+      final file = await _getFile();
+      if (!file.existsSync()) return [];
+      final jsonData = await file.readAsString();
+      final List<dynamic> jsonList = json.decode(jsonData);
+      return jsonList.map((item) => _fromJson(item)).toList().cast<T>();
+    } catch (e) {
+      return [];
+    }
+  }
+
+  Future<void> create(T data) async {
+    try {
+      final file = await _getFile();
+      List<T> dataList = await readAll();
+      dataList.add(data);
+      final jsonData =
+          json.encode(dataList.map((item) => _toJson(item)).toList());
+      await file.writeAsString(jsonData);
+    } catch (e) {
+      if (kDebugMode) {
+        print("Error creating data: $e");
+      }
+    }
+  }
+
+  Future<void> update(T oldData, T newData) async {
+    try {
+      final file = await _getFile();
+      List<T> dataList = await readAll();
+      final index = dataList.indexOf(oldData);
+      if (index != -1) {
+        dataList[index] = newData;
+        final jsonData =
+            json.encode(dataList.map((item) => _toJson(item)).toList());
+        await file.writeAsString(jsonData);
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print("Error updating data: $e");
+      }
+    }
+  }
+
+  Future<void> delete(T data) async {
+    try {
+      final file = await _getFile();
+      List<T> dataList = await readAll();
+      dataList.remove(data);
+      final jsonData =
+          json.encode(dataList.map((item) => _toJson(item)).toList());
+      await file.writeAsString(jsonData);
+    } catch (e) {
+      if (kDebugMode) {
+        print("Error deleting data: $e");
+      }
+    }
+  }
+
+  dynamic _toJson(T data) {
+    return data;
+  }
+
+  T _fromJson(dynamic json) {
+    return json as T;
+  }
 }
